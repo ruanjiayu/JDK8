da17b7a439f145a5fb703bf5b358f15700b5ba66
ruanjiayu <1005572910@qq.com>
2020-07-05 17:12:48 +0800

    完成java8的相关补充

--- src/test/java/com/xian/jdk/bo/StudentTest.java
+++ src/test/java/com/xian/jdk/bo/StudentTest.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 import java.util.List;
-import java.util.Optional;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -95,8 +94,11 @@ public class StudentTest {
     @Test
     public void sortBaseTest() {
         List<String> list = Arrays.asList("2", "3", "1", "2", "5", "4");
-        list.stream().sorted().forEach(System.out::println);
-        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        list.stream().sorted().forEach(System.out::print);
+        System.out.println();
+        list.stream().sorted(Comparator.naturalOrder()).forEach(System.out::print);
+        System.out.println();
+        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);
     }
 
     /**
