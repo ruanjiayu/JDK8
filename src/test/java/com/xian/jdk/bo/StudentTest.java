b5fdafec73b766fb2fbf8c17907a0967a73a7cbb
ruanjiayu <1005572910@qq.com>
2020-07-05 17:08:08 +0800

    完成java8的相关补充

--- src/test/java/com/xian/jdk/bo/StudentTest.java
+++ src/test/java/com/xian/jdk/bo/StudentTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @Description:
@@ -113,7 +117,8 @@ public class StudentTest {
 //        students.stream().sorted((stu1, stu2) -> Long.compare(stu1.getId(), stu2.getId())).forEach(System.out::println);
 //        students.stream().sorted(Comparator.comparingLong(Student::getId)).forEach(System.out::println);
         students.stream()
-                .sorted((stu1, stu2) -> Long.compare(stu2.getId(), stu1.getId()))
+//                .sorted((stu1, stu2) -> Long.compare(stu2.getId(), stu1.getId())) 逆序排序
+                .sorted(Comparator.comparingLong(Student::getId).reversed())
                 .sorted(Comparator.comparingInt(Student::getAge))
                 .forEach(System.out::println);
     }
@@ -204,6 +209,117 @@ public class StudentTest {
             System.out.println("【没有叫杨洋的】");
         }
     }
+
+    /**
+     * findFirst 查询第一个找到的内容
+     * findAny 查询任意一个
+     */
+    @Test
+    public void findTest() {
+        Student s1 = new Student(1L, "肖战", 15, "浙江");
+        Student s2 = new Student(2L, "王一博", 15, "湖北");
+        Student s3 = new Student(3L, "杨洋", 17, "北京");
+        Student s4 = new Student(4L, "李现", 17, "浙江");
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        Student student = students.stream().findFirst().get();
+        System.out.println(student);
+        Student student1 = students.stream().findAny().get();
+        System.out.println(student1);
+    }
+
+    /**
+     * flatMap 将流中内容映射成一个流，在对多个流处理时候有效
+     */
+    @Test
+    public void flatMapTest() {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Integer> list1 = Arrays.asList(3, 4);
+        List<int[]> pairs = list.stream().flatMap(i -> list1.stream().map(j -> new int[]{i, j})).collect(Collectors.toList());
+        pairs.stream().forEach(pair -> System.out.println(Arrays.toString(pair)));
+    }
+
+    /**
+     * 数组转换成流
+     */
+    @Test
+    public void arraysTest() {
+        String[] arrayOfWords = {"goodbye", "world"};
+        Stream<String> stream = Arrays.stream(arrayOfWords);
+    }
+
+
+    /**
+     * mapToInt映射成特定的流
+     */
+    @Test
+    public void mapToIntTest() {
+        Student s1 = new Student(1L, "肖战", 15, "浙江");
+        Student s2 = new Student(2L, "王一博", 15, "湖北");
+        Student s3 = new Student(3L, "杨洋", 17, "北京");
+        Student s4 = new Student(4L, "李现", 17, "浙江");
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        System.out.println(students.stream().mapToInt(Student::getAge).sum());
+        // 获取所有相关值(最大，最小，平均，总数，求和)
+        IntSummaryStatistics intSummaryStatistics = students.stream().mapToInt(Student::getAge).summaryStatistics();
+        System.out.println(intSummaryStatistics);
+    }
+
+
+    /**
+     * 获取范围内的值
+     */
+    @Test
+    public void rangeClosedTest() {
+        System.out.println(IntStream.range(1, 100).sum());
+        System.out.println(IntStream.rangeClosed(1, 100).sum());
+        // 装箱过程，不能直接collect
+        IntStream.range(0, 10).boxed().collect(Collectors.toList());
+    }
+
+    /**
+     * 将字符串组合起来
+     */
+    @Test
+    public void joiningTest() {
+        Student s1 = new Student(1L, "肖战", 15, "浙江");
+        Student s2 = new Student(2L, "王一博", 15, "湖北");
+        Student s3 = new Student(3L, "杨洋", 17, "北京");
+        Student s4 = new Student(4L, "李现", 17, "浙江");
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        System.out.println(students.stream().map(Student::getName).collect(Collectors.joining(", ")));
+    }
+
+    /**
+     * 将内容进行map操作
+     */
+    @Test
+    public void groupingByTest() {
+        Student s1 = new Student(1L, "肖战", 15, "浙江");
+        Student s2 = new Student(2L, "王一博", 15, "湖北");
+        Student s3 = new Student(3L, "杨洋", 17, "北京");
+        Student s4 = new Student(4L, "李现", 17, "浙江");
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        Map<Integer, List<Student>> collect = students.stream().collect(Collectors.groupingBy(Student::getAge));
+        System.out.println(collect);
+    }
+
+
 
 
 }
