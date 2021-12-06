dc945188fe58e9383793acd042f0041b936f7959
summer <ruanjiayu@fenxianglife.com>
2021-12-06 14:50:28 +0800

    完成链接匹配规则

--- src/test/java/com/xian/jdk/regex/README.md
+++
@@ -1,4 +0,0 @@
-https://juejin.cn/post/6844903487155732494#heading-30
-
-https://juejin.cn/post/6844903845227659271
-
--- src/test/java/com/xian/jdk/regex/RegexTest.java
+++ src/test/java/com/xian/jdk/regex/RegexTest.java
@@ -1,5 +1,10 @@
 package com.xian.jdk.regex;
 
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @Description: 正则表达式
  * @Author: Summer
@@ -9,8 +14,52 @@ package com.xian.jdk.regex;
 public class RegexTest {
 
 
-    public static void main(String[] args) {
-
+    /**
+     * 匹配链接内是否存在URL
+     */
+    @Test
+    public void test1() {
+        String str = "阮佳裕最喜欢的链接http://www.baidu.com哈哈哈哈哈你好";
+        String regex = "(https?|http?)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+            System.out.println(matcher.groupCount());
+        }
     }
 
+    /**
+     * 匹配文字中的链接
+     */
+    @Test
+    public void test2() {
+        String str = "阮佳裕京东https://item.jd.com/23247384728哈哈哈哈，我很喜欢";
+        String regex = "https://(u.jd|item.jd|item.m.jd).com/[a-zA-Z0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.groupCount());
+        }
+    }
+
+
+
+    /**
+     * 匹配文字中的链接
+     */
+    @Test
+    public void test3() {
+        String str = "阮佳裕京东https://detail.tmall.com?id=23247384728哈哈哈哈，我很喜欢";
+        String regex = "detail.tmall.*(\\?id|\\&id)=([0-9]+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.group(matcher.groupCount()));
+        }
+    }
 }
